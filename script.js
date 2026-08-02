9ce1193fbc9ede4f20c42c7625c6a19c6fe88b54
AVIN_K_JOY <63731038+AVINKOKKENJOY@users.noreply.github.com>
2026-08-02 14:02:34 +0530

    njknb

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// Force scroll to top on refresh
 if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; }
+
+// 1. PETAL GENERATOR
 function createPetals() {
     const container = document.getElementById('petal-container');
     for (let i = 0; i < 40; i++) {
@@ -12,14 +15,18 @@ function createPetals() {
         container.appendChild(petal);
     }
 }
+
+// 2. ENTRANCE & MUSIC
 document.getElementById('startBtn').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'instant' });
     const music = document.getElementById('bgMusic');
-    music.play().catch(e => console.log("Music Ready"));
+    music.play().catch(e => console.log("Music unlocked."));
     document.getElementById('entrance-overlay').classList.add('hide-overlay');
     createPetals();
     setTimeout(() => { AOS.init({ duration: 1200, once: true }); }, 500);
 });
+
+// 3. COUNTDOWN
 const engagementDate = new Date("August 25, 2024 18:00:00").getTime();
 function updateTimer() {
     const now = new Date().getTime();
